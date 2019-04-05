81162b3f05db0631dcbc764057e651ddcc0f7d30
RayanHirech <rhirech059@student.fuhsd.org>
2019-04-04 21:46:23 -0700

    Do Networking Homework.

--- src/Client.java
+++ src/Client.java
@@ -2,6 +2,7 @@ import javax.swing.*;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.PrintWriter;
 import java.net.Socket;
 
 public class Client {
@@ -14,10 +15,22 @@ public class Client {
         Socket socket = new Socket(SERVER_IP, SERVER_PORT);
 
         BufferedReader input = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+        BufferedReader keyboard = new BufferedReader(new InputStreamReader(System.in));
+        PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
 
-        String serverResponse = input.readLine();
+        while (true) {
+            System.out.println("> ");
+            String command = keyboard.readLine();
 
-        JOptionPane.showMessageDialog(null, serverResponse);
+            if (command.equals("quit")) {
+                break;
+            }
+
+            out.println(command);
+
+            String serverResponse = input.readLine();
+            System.out.println("Server says: " + serverResponse);
+        }
 
         socket.close();
         System.exit(0);
